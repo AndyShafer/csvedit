e9db12debe775a5907dfe39661c5d614e2bd0adf
andyshafer <andyshafer0715@gmail.com>
2020-09-03 00:00:40 -0400

    Implemented function for parsing fields from line

--- src/io/csvreader.cpp
+++ src/io/csvreader.cpp
@@ -17,5 +17,36 @@ bool CSVReader::readline(std::string& line) {
 }
 
 bool CSVReader::readline(std::vector<std::string>& fields) {
-	return false;
+	if(!file.is_open()) {
+		return false;
+	}
+
+	std::string line;
+	if(!getline(file, line)) {
+		file.close();
+		return false;
+	}
+
+	std::vector<int> delimeters;
+	bool quoted = false;
+	for(int i = 0; i < line.size(); i++) {
+		if(line[i] == '"' && (i == 0 || line[i-1] != '\\')) {
+			quoted = !quoted;
+		}
+		if(line[i] == ',' && !quoted) {
+			delimeters.push_back(i);
+		}
+	}
+
+	if(delimeters.size() == 0) {
+		fields.push_back(line);
+		return true;
+	}
+
+	fields.push_back(line.substr(0, delimeters[0]));
+	for(int i = 0; i < delimeters.size()-1; i++) {
+		fields.push_back(line.substr(delimeters[i] + 1, delimeters[i+1] - delimeters[i] - 1));
+	}
+	fields.push_back(line.substr(delimeters[delimeters.size()-1]));
+	return true;
 }
